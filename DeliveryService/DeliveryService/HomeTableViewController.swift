71cca77b9c330e9745e30fb0d6976adaf92afb55
pastapeter <pastapeter@naver.com>
2021-02-27 22:52:03 +0900

    making custom table cellt

---
+++ DeliveryService/DeliveryService/HomeTableViewController.swift
@@ -0,0 +1,91 @@
+//
+//  HomeTableViewController.swift
+//  DeliveryService
+//
+//  Created by abc on 2021/02/26.
+//
+
+import UIKit
+
+var storeArray = ["슬슬브라우니", "아름돈카츠", "선데이밀 강남점", "바이러브하니"]
+var storeImageArray = ["1.jpeg", "2.jpeg", "3.jpeg", "4.jpeg", "5.jpeg", "6.jpeg", "7.jpeg", "8.jpeg", "9.jpeg", "10.jpeg"]
+class HomeTableViewController: UITableViewController{
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return storeArray.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath) as! tableViewCellTableViewCell
+        cell.lblstoreName.text = storeArray[indexPath.row]
+        cell.storeImgView.image = UIImage(named: storeImageArray[indexPath.row])
+        return cell
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
---
+++ DeliveryService/DeliveryService/tableViewCellTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  tableViewCellTableViewCell.swift
+//  DeliveryService
+//
+//  Created by abc on 2021/02/26.
+//
+import UIKit
+
+class tableViewCellTableViewCell: UITableViewCell {
+
+    @IBOutlet var storeImgView: UIImageView!
+    @IBOutlet var lblstoreName: UILabel!
+    @IBOutlet var lblPoint: UILabel!
+    @IBOutlet var lblDistance: UILabel!
+    @IBOutlet var lblPeopleNum: UILabel!
+    @IBOutlet var lblperson1: UILabel!
+    @IBOutlet var lblperson2: UILabel!
+    @IBOutlet var btnMorePeople: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
